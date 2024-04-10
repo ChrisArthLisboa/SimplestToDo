e666cb000b95bac779e5ea694b1970f4182562b2
ChrisArthLisboa <19lisboa19@gmail.com>
2024-04-10 15:06:09 -0300

    :feat: correction of checker and set_up

---
+++ build/src/feats/CMakeFiles/feats.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/feats.dir/main.c.o"
+  "CMakeFiles/feats.dir/main.c.o.d"
+  "libfeats.a"
+  "libfeats.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/feats.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/feats/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lisboa/Documents/learning/SimplestToDo/src/feats
+# Build directory: /home/lisboa/Documents/learning/SimplestToDo/build/src/feats
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
