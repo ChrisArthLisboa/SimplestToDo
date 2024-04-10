476e1d1906346f7ddaca17e50305a2cf3e735b2e
Lisboa <131053411+ChrisArthLisboa@users.noreply.github.com>
2024-04-10 18:07:43 +0000

    Delete build directory

--- build/src/feats/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lisboa/Documents/learning/SimplestToDo/src/feats
-# Build directory: /home/lisboa/Documents/learning/SimplestToDo/build/src/feats
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
