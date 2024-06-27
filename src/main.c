afaf9e3a660cd0bfd4af1d1ae56e1e61c8820afa
ChrisArthLisboa <19lisboa19@gmail.com>
2024-06-27 12:17:39 -0300

    :fix: for test

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ int main(int argc, char **argv) {
         printf("creating files\n");
     }
 
+    printf("%d\n", argc);
     if (argc <= 2) {
         printf("Please run with a flag.\n");
         help_cmd();
@@ -25,11 +26,12 @@ int main(int argc, char **argv) {
     }
 
     GtkApplication *app;
+    char** false_argv;
     int status;
 
     app = gtk_application_new("org.lisboa.SimplestTodo", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK( activate_main ), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
+    status = g_application_run(G_APPLICATION(app), 1, false_argv);
     g_object_unref(app);
 
     return status;
